a2c714e46be5b3b6e35ebf33e6a573572b50d417
DoMa12 <96224940+DoMa12@users.noreply.github.com>
2021-12-26 12:33:49 +0100

    Add files via upload

---
+++ x.c
@@ -0,0 +1,23 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    int F1;
+    int F2;
+    scanf("%d",&F1);
+    scanf("%d",&F2);
+    int Fuk=F1+F2;
+    printf("Ukupna sila %d\n",Fuk);
+    int S1;
+    int S2;
+    scanf("%d",&S1);
+    scanf("%d",&S2);
+    int Suk=S1+S2;
+     printf("Presjek %d\n",Suk);
+     int v=Fuk+Suk;
+     printf("V %d\n",Fuk/Suk);
+
+     return 0;
+
+}
